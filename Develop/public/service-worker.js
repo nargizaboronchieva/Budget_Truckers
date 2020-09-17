7fffa9b532d3bb7d5a0850ba55b2fd4cb449915f
Nargiza Boronchieva <nargizaboronchieva@gmail.com>
2020-09-17 00:35:34 -0400

    added indexeddb to store data while offline

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -1,4 +1,11 @@
-const FILES_TO_CACHE = ["/", "/index.html", "/index.js", "/favicon.ico","/styles.css","/icons/icon-192x192.png", "/icons/icon-512x512.png"];
+const FILES_TO_CACHE = [
+  "/", 
+  "/index.html",
+  "/index.js",
+  "/favicon.ico",
+  "/styles.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png"];
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
--- Develop/seeders/seed.js
+++
@@ -1,5 +0,0 @@
-const mongoose =require("mongoose");
-const db =require("../models");
-mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/imageperformance",{
-    useNewURLParser: true
-});
