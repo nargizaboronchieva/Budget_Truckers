09f17bf79f85cc223671ce7c69f72a8dc7e054a5
Nargiza Boronchieva <nargizaboronchieva@gmail.com>
2020-09-17 01:09:55 -0400

    added code to return the cached home page for all requests for html pages

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -62,4 +62,17 @@ self.addEventListener("fetch", function(evt) {
     );
 
     return;
-}});
+  }
+  evt.respondWith(
+    fetch(evt.request).catch(function () {
+      return caches.match(evt.request).then(function (response) {
+        if (response) {
+          return response;
+        } else if (evt.request.headers.get("accept").includes("text/html")) {
+          // return the cached home page for all requests for html pages
+          return caches.match("/");
+        }
+      });
+    })
+  );
+});
