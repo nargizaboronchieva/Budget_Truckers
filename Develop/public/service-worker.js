555e523941c6437c3e498812d5e96241f225eb86
Nargiza Boronchieva <nargizaboronchieva@gmail.com>
2020-09-16 23:58:06 -0400

    setting up cache

---
+++ Develop/public/service-worker.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/favicon.ico",
+  "/manifest.webmanifest",
+  "/assets/css/style.css",
+  "/assets/js/loadImages.js",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+
+];
